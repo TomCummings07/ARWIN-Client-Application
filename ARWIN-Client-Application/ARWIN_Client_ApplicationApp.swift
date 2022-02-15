75b3075ad6d62c76fd5226890d7ea538d98dfb00
Tom Cummings <tom.cummings07@outlook.com>
2022-02-15 11:49:51 +0000

    Initial Commit

---
+++ ARWIN-Client-Application/ARWIN_Client_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARWIN_Client_ApplicationApp.swift
+//  ARWIN-Client-Application
+//
+//  Created by Tom Cummings on 15/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ARWIN_Client_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
